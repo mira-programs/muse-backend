07f3e7ec37c35dc9d83fb992d9463561bd9ceda8
mira-programs <mirahussein06@gmail.com>
2024-03-26 13:20:57 +0200

    clean

--- MUSE/src/main/java/com/example/demo/registration/token/VerificationTokenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.registration.token;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
-    VerificationToken findByToken(String token);
-}
